69d507d7d983772521095975d7665b3c785051c6
swelbourn <welbourn.stephen@gmail.com>
2021-02-03 15:37:25 -0600

    create footer component and styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.scss';
 import { Splashpage } from './pages/splash/splash.component';
 import { TopNav } from './components/TopNav.component';
 import Homepage from './pages/homepage/homepage.component';
+import { Footer } from './components/Footer/Footer.component';
 
 function App() {
   return (
     <div className="app">
       <TopNav />
       <Homepage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Footer.styles.scss';
+
+export const Footer = () => (
+  <footer>
+    <nav>
+      <p>Copyright blahdeeblah</p>
+      <p>
+        <a href="">About</a>
+        <a href="">Support</a>
+        <a href="">Careers</a>
+        <a href="">Press</a>
+        <a href="">Investors</a>
+        <a href="">Privacy</a>
+        <a href="">Developers</a>
+        <a href="">Terms</a>
+        <a href="">Cookies</a>
+      </p>
+    </nav>
+  </footer>
+);
