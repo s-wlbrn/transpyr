630fffea53cb0bae47ade91959f3b76728cc1cee
swelbourn <welbourn.stephen@gmail.com>
2021-09-19 05:37:09 -0500

    fix getMyBookings API caller function

--- src/api.js
+++ src/api.js
@@ -130,8 +130,8 @@ class API {
   };
 
   //get own bookings
-  getMyBookings = async (options = {}) => {
-    const response = await this.getAll('/bookings/me', options.query);
+  getMyBookings = async (query) => {
+    const response = await this.getAll('/bookings/me', query);
     return response.data;
   };
 
